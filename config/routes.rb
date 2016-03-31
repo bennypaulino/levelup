a96e24ddf93f6d989759c89cc1eb53d7272cb2f4
Benny Paulino <santiagopro71@gmail.com>
2016-03-31 00:17:03 +0000

    Allow students to see what courses are available; generate a controller for courses; adjust config/routes.rb; adjust app/controllers/courses_controller.rb; create app/views/courses/index.html.erb; adjust app/views/layouts/application.html.erb; adjust app/views/static_pages/index.html.erb; and adjust master.css.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Levelup::Application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show, :edit, :update, :destroy]
   end
