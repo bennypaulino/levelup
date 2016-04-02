8a1b709681954b0a6e1225a9abca1c79bebacd23
Benny Paulino <santiagopro71@gmail.com>
2016-04-02 23:25:30 +0000

    Lock down the instructor course page and set up permissions to make sure the user who created the page only has access to the instructor's course show page.

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -15,7 +16,6 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   def edit
@@ -52,6 +52,17 @@ class Instructor::CoursesController < ApplicationController
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
