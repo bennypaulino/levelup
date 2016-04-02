25ed33978eab68e256e40e39fe260b3c08261279
Benny Paulino <santiagopro71@gmail.com>
2016-04-02 01:30:26 +0000

    Set up permissions for Sections, so that they are secured in the same manner as Lessons.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Levelup::Application.routes.draw do
   resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create, :edit, :update]
+      resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show, :edit, :update, :destroy] do
       resources :sections, only: [:new, :create]
